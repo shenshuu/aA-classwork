57aab02e06fe6c1b70de8375126ee7d990290209
shenshuu <michaelshen85@gmail.com>
2022-06-12 03:46:47 -0400

    Added valid_move? method

--- W3D4/minesweeper/minesweeper.rb
+++ W3D4/minesweeper/minesweeper.rb
@@ -3,13 +3,34 @@ class Minesweeper
 
     def initialize
         @game = Board.new 
+        @remaining_spaces = @game.remaining_spaces
     end
 
     def flag(pos)
         @game[pos].flag = !@game[pos].flag
     end
 
-    def play 
-
+    def make_move 
+        pos = nil 
+        until pos && valid_move?(pos)
+            begin 
+                puts "Please enter a valid position: "
+                pos = gets.chomp.split(" ").map {|ele| ele.to_i}
+            rescue
+                pos = nil 
+            end
+        end
+        pos 
     end
+
+    def valid_move?(move)
+        move.is_a?(Array) &&
+        move[0].is_a?(Integer) &&
+        move[1].is_a?(Integer) &&
+        @game.valid_pos?(move)
+    end
+
+    # def play 
+
+    # end
 end 
