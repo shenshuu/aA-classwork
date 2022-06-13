2ecf1f2279a6b7f221199712864313b248768618
shenshuu <michaelshen85@gmail.com>
2022-06-13 11:38:03 -0400

    Added find_path method

--- W3D5/knights.rb
+++ W3D5/knights.rb
@@ -1,15 +1,70 @@
 require_relative "treenode.rb"
-
+require "set"
 class KnightPathFinder
-
+    
     @@offsets = [[2,1], [1,2], [-1,2], [-2,1], [-2,-1], [-1,-2], [1,-2], [2,-1]]
-    def self.valid_moves(pos)
+    
+    def initialize(pos)
         @considered_positions = [pos]
+        @pos = pos 
+    end
+    
+    def self.valid_moves(pos)
+        @@offsets.map {|offset| [pos[0]+offset[0], pos[1]+offset[1]]}
     end 
 
+    def valid_pos?(pos)
+        row, col = pos 
+        row >= 0 && row <= 7 && col >= 0 && col <= 7
+    end
+
     def new_move_positions(pos)
-        #KnightPathFinder.valid_moves(pos)
-        new_positions = @@offsets.map {|offset| [pos[0]+offset[0], pos[1]+offset[1]]}
-        new_positions = new_positions.reject {|position| @considered_positions.include?(position)}
+        new_positions = KnightPathFinder.valid_moves(pos)
+        new_positions = new_positions.reject {|position| @considered_positions.include?(position) || !valid_pos?(position)}
+        new_positions.each {|position| @considered_positions << position}
+        new_positions
+    end
+
+    def build_move_tree 
+        root = PolyTreeNode.new(@pos)
+        seen_pos = Set.new(@pos)
+        head = root 
+        queue = [root]
+
+        until queue.empty?
+            root = queue.shift 
+            new_positions = new_move_positions(root.value)
+            new_positions.each do |pos|
+                next if seen_pos.include?(pos)
+                child = PolyTreeNode.new(pos)
+                child.parent = root 
+                queue << child 
+                seen_pos.add(pos)
+                p child 
+            end
+        end
+        head 
+    end
+
+    def find_path(end_pos)
+        root = build_move_tree
+        node = root.dfs(end_pos)
+        trace_path_back(node)
+    end
+    
+    def trace_path_back(node)
+        path = []
+        until node.parent.nil?
+            path << node.value
+            node = node.parent
+        end
+        path.reverse
     end
 end
+
+k = KnightPathFinder.new([0,0])
+k.new_move_positions([0,0])
+# k.build_move_tree
+s = Set.new
+s.add([0,0])
+p s.include?([0,0])
