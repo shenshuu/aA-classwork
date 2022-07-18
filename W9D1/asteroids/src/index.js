c1d7c3317196d7feb91d579c338b57ec94e14505
Michael Shen <michaelshen85@gmail.com>
2022-07-18 10:05:29 -0400

    made a circle

--- W9D1/asteroids/src/index.js
+++ W9D1/asteroids/src/index.js
@@ -1,1 +1,11 @@
-console.log("test")
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "red"
+  });
+
+mo.draw(mo);
--- W9D1/asteroids/src/moving_object.js
+++ W9D1/asteroids/src/moving_object.js
@@ -1,6 +1,19 @@
-function MovingObject(pos, vel, radius, color) {
-    this.pos = pos;
-    this.vel = vel;
-    this.radius = radius;
-    this.color = color;
+function MovingObject(options) {
+    this.pos = options.pos;
+    this.vel = options.vel;
+    this.radius = options.radius;
+    this.color = options.color;
 };
+
+MovingObject.prototype.draw = function(ctx) {
+    
+    const canvas = document.getElementById('game-canvas');
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2*Math.PI);
+    ctx.fill();
+}
+
+module.exports = MovingObject;
+
